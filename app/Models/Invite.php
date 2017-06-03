f19b637ae504409dd5bd8af99ebb958c5246b7ec
cbaconnier <clement.baconnier@gmail.com>
2017-06-03 14:41:13 +0200

    update models phpdoc

--- app/Models/Invite.php
+++ app/Models/Invite.php
@@ -4,6 +4,27 @@ namespace App\App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\App\Models\Invite
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $email
+ * @property string $token
+ * @property int $invited_by
+ * @property int $claimed_by
+ * @property string $claimed_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereClaimedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereClaimedBy($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereInvitedBy($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\App\Models\Invite whereUpdatedAt($value)
+ */
 class Invite extends Model
 {
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,40 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\Models\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property string $birth_date
+ * @property string $address1
+ * @property string $address2
+ * @property string $postcode
+ * @property string $city
+ * @property string $country
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereAddress1($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereAddress2($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereBirthDate($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereCity($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereCountry($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereFirstName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereLastName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User wherePostcode($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\User whereUpdatedAt($value)
+ */
 class User extends Authenticatable
 {
     use Notifiable;
